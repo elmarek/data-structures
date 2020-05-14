8f483fcbb934f4431b6f2d182bde062c27b4740d
elmarek <mareks@gmx.com>
2020-05-13 21:39:01 -0700

    Complete es6 pattern

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,21 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+  }
+  enqueue (value) {
+    return this.storage[value] = value;
+  }
+
+  dequeue () {
+    var first = Object.keys(this.storage)['0'];
+    var deletedValue = this.storage[first];
+    delete this.storage[first];
+    return deletedValue;
+  }
+
+  size () {
+    return Object.keys(this.storage).length;
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,27 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+  }
+
+  push (value) {
+    var defaultValue = Object.keys(this.storage).length;
+    while (this.storage.defaultValue !== undefined) {
+      defaultValue++;
+    }
+    return this.storage[defaultValue] = value;
+  }
+
+  pop () {
+    var last = [Object.keys(this.storage)[Object.keys(this.storage).length - 1]];
+    var deletedValue = this.storage[last];
+    delete this.storage[last];
+    return deletedValue;
+  }
+
+  size () {
+    return Object.keys(this.storage).length;
   }
 
 }
+
