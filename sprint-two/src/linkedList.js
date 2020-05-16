bbdf6d2d25e25f7ed0e23ac0b366b325a2f890c5
elmarek <mareks@gmx.com>
2020-05-16 15:41:02 -0700

    Working data structures all

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,58 @@
+
 var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  var previousNode = null;
 
   list.addToTail = function(value) {
+    var nodeObj = new Node(value);
+
+    //if list.head === null, do
+    if (list.head === null) {
+      //set nodeObj to list.head
+      list.head = nodeObj;
+      list.tail = nodeObj;
+      previousNode = nodeObj;
+      // console.log("PreviousNode :", previousNode);
+
+    } else if (list.head) {
+
+      previousNode.next = nodeObj;
+      // console.log('SecondNode: ', previousNode);
+      previousNode = nodeObj;
+      list.tail = nodeObj;
+    }
   };
 
   list.removeHead = function() {
+    if (list.head) {
+      var result = list.head.value;
+      // console.log("result :", result);
+      list.head = list.head.next;
+    }
+
+    return result;
   };
 
   list.contains = function(target) {
+    var result;
+
+    var checkNext = function(obj) {
+      if (obj.value === target) {
+        result = true;
+
+      } else if (obj.next !== null) {
+        checkNext(obj.next);
+
+      } else {
+        result = false;
+      }
+    };
+
+    checkNext(list.head);
+
+    return result;
   };
 
   return list;
@@ -26,4 +69,5 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ O(n) is the worse case time complexity since we have to recursively iterate through each linked object in order to find the target value.
  */
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,23 +1,36 @@
+
 var Tree = function(value) {
-  var newTree = {};
+  var newTree = Object.create(treeMethods);
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var newChild = Tree(value);
+  this.children.push(newChild);
 };
 
 treeMethods.contains = function(target) {
+  var result = false;
+  var childLookup = function(obj) {
+    if (obj !== undefined && obj.length > 0) {
+      for (var i = 0; i < obj.length; i++) {
+        if (obj[i].value === target) {
+          result = true;
+        } else if (obj[i].children) {
+          childLookup(obj[i].children);
+        }
+      }
+    }
+  };
+  childLookup(this.children);
+  return result;
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * Answer: O(n) is the worst case complexity since iteration is required in the contains method.
  */
